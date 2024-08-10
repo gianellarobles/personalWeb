8a2912ff0b5d09ffb3fefcbeb0aa6dc1568d2c4c
gianellarobles <gianellarobles@gmail.com>
2024-08-10 00:52:25 -0400

    Replaced Icon
    changed an old icon to a new one

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,7 +3,6 @@ import WelcomeItem from './WelcomeItem.vue'
 import DocumentationIcon from './icons/IconDocumentation.vue'
 import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 </script>
 
@@ -58,9 +57,7 @@ import SupportIcon from './icons/IconSupport.vue'
   </WelcomeItem>
 
   <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
+    
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
