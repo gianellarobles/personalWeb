8a211806ab78539b041615be5971aed466064015
gianellarobles <gianellarobles@gmail.com>
2024-08-10 14:42:05 -0400

    Update HelloWorld.vue
    updated the css of the welcome page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 defineProps<{
   msg: string
 }>()
@@ -7,34 +8,49 @@ defineProps<{
 
 <template>
   <div class="greetings">
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+ 
     <h1 class="is-size-1 has-text-weight-normal has-text-white">{{ msg }} <span class="is-italic pink">Gianella</span></h1> 
    <h3>
   Feel free to browse through my personal website about my passions, projects, and resume.</h3>
-    
+<br>
+<br>
+  <img src="/Gianella.jpg">
+
   </div>
 </template>
 
 <style scoped>
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+/* this is the css for app */
+img{
+  height: 450px;
+  width: 450px;
+  border-radius: 50%;
+  display: block;
+  margin: auto;
+  margin-top: -620px;
 }
 
+.greetings h1{
+  text-align: center;
+  margin-top: 575px;
+}
+.greetings h3 {
+  text-align: center;
+  
+}
+
+/* this is the css when full screen */ 
 @media (min-width: 1024px) {
-  .greetings h1,
+  .greetings h1{
+    text-align: left;
+    margin-top: 300px;
+  }
   .greetings h3 {
     text-align: left;
-
+  }
+  img{
+    margin-top: -275px;
+    float: right;
   }
 }
 </style>
